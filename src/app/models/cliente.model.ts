17384a20c611497a2903b1dd1c326a51f7a5213e
Joaquin Martinez <joaquinivanmartinez@gmail.com>
2020-08-19 14:57:10 -0300

    creando modelo cliente

---
+++ src/app/models/cliente.model.ts
@@ -0,0 +1,11 @@
+export class cliente{
+    nombre: string;
+    apellido: string;
+    mail: string;
+
+    constructor(nomb:string; ape:string; mail:string){
+        this.nombre = nomb;
+        this.apellido = ape;
+        this.mail = mail;
+    }
+}
